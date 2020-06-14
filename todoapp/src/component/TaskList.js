8b3926cefbe09b18a668435d7de798fa8913a764
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-14 20:26:10 +0200

    project done

--- todoapp/src/component/TaskList.js
+++ todoapp/src/component/TaskList.js
@@ -5,7 +5,32 @@ const TaskList = (props) => {
 
     const active = props.tasks.filter(task => task.active);
     const done = props.tasks.filter(task => !task.active);
-    console.log(active, done)
+
+    // done.sort((a, b) => b.finishDate - a.finishDate)
+
+    if (done.length >= 2) {
+        done.sort((a, b) => {
+            if (a.finishDate < b.finishDate) {
+                return 1
+            }
+            if (a.finishDate > b.finishDate) {
+                return -1
+            }
+            return 0
+        })
+    }
+
+    if (active.length >= 2) {
+        active.sort((a, b) => {
+
+            a = a.text.toLowerCase();
+            b = b.text.toLowerCase();
+
+            if (a.text < b.text) return -1;
+            if (a.text > b.text) return 1;
+            return 0
+        })
+    }
 
     const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
     const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
